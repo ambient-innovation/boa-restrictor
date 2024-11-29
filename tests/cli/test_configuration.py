83717ac0f4db6bf4755d9b5ece3ba2f1aa69113b
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2024-11-29 15:00:48 +0100

    test fix

--- tests/cli/test_configuration.py
+++ tests/cli/test_configuration.py
@@ -5,19 +5,14 @@ if sys.version_info >= (3, 11):
     import tomllib
 else:
     import tomli as tomllib
-from pathlib import Path
 from unittest import mock
 
 from boa_restrictor.cli.configuration import load_configuration
 
-EXISTING_FILE = Path.cwd().parent.parent / "pyproject.toml"
-
 
 @mock.patch.object(tomllib, "load", return_value={"tool": {"boa-restrictor": {"exclude": ["PBR001"]}}})
 def test_load_configuration_happy_path(mocked_load):
-    assert os.path.isfile(EXISTING_FILE)
-
-    data = load_configuration(file_path=EXISTING_FILE)
+    data = load_configuration(file_path=os.path.abspath(sys.argv[0]))
 
     mocked_load.assert_called_once()
     assert data == {"exclude": ["PBR001"]}
@@ -33,7 +28,7 @@ def test_load_configuration_invalid_file(mocked_load):
 
 @mock.patch.object(tomllib, "load", return_value={"tool": {"other_linter": True}})
 def test_load_configuration_key_missing(mocked_load):
-    data = load_configuration(file_path=EXISTING_FILE)
+    data = load_configuration(file_path=os.path.abspath(sys.argv[0]))
 
     mocked_load.assert_called_once()
     assert data == {}
