b131957ae41070bca3bf86080843b8f51aeaf92b
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-01-13 15:25:08 +0100

    Fixed test path

--- tests/rules/test_init.py
+++ tests/rules/test_init.py
@@ -8,7 +8,7 @@ def test_boa_restrictor_rules_constant_not_missing_rules():
     This test is a check to ensure we don't forget to register new rules.
     """
     number_of_rule_files = 0
-    for file in Path("../../boa_restrictor/rules").iterdir():
+    for file in (Path(__file__).resolve().parent.parent.parent / "boa_restrictor/rules").iterdir():
         if file.suffix == ".py" and file.name != "__init__.py":
             number_of_rule_files += 1
 
