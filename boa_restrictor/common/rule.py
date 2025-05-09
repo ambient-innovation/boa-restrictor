986d21416552ab4d31d40076b46ac6737a4123a9
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-05-09 08:18:37 +0200

    v1.5.2

--- boa_restrictor/common/rule.py
+++ boa_restrictor/common/rule.py
@@ -1,5 +1,6 @@
 from _ast import AST, Module
 from pathlib import Path
+from typing import Union
 
 from boa_restrictor.projections.occurrence import Occurrence
 
@@ -16,7 +17,7 @@ class Rule:
     source_tree: AST
 
     @classmethod
-    def run_check(cls, *, file_path: Path, source_tree: AST | Module) -> list[Occurrence]:
+    def run_check(cls, *, file_path: Path, source_tree: Union[AST, Module]) -> list[Occurrence]:
         instance = cls(file_path=file_path, source_tree=source_tree)
         return instance.check()
 
