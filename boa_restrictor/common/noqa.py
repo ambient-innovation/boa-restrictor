682b927ef1816cb6ec3ca3483344b3090e7eb487
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-05-09 08:40:02 +0200

    #27: DBR rules not ignorable via "noqa"

--- boa_restrictor/common/noqa.py
+++ boa_restrictor/common/noqa.py
@@ -2,7 +2,7 @@ import re
 import tokenize
 from io import StringIO
 
-from boa_restrictor.common.rule import PYTHON_LINTING_RULE_PREFIX
+from boa_restrictor.common.rule import DJANGO_LINTING_RULE_PREFIX, PYTHON_LINTING_RULE_PREFIX
 
 
 def get_noqa_comments(*, source_code: str) -> list[tuple[int, str]]:
@@ -12,12 +12,16 @@ def get_noqa_comments(*, source_code: str) -> list[tuple[int, str]]:
     noqa_statements = []
 
     tokens = tokenize.generate_tokens(StringIO(source_code).readline)
-    pattern = re.compile(r"^#\snoqa:\s*.*?" + PYTHON_LINTING_RULE_PREFIX + r"\d{3}")
+    patterns = [
+        re.compile(r"^#\snoqa:\s*.*?" + PYTHON_LINTING_RULE_PREFIX + r"\d{3}"),
+        re.compile(r"^#\snoqa:\s*.*?" + DJANGO_LINTING_RULE_PREFIX + r"\d{3}"),
+    ]
 
     for token in tokens:
         token_type, token_string, start, _, _ = token
 
-        if token_type == tokenize.COMMENT and pattern.search(token_string):
-            noqa_statements.append((start[0], token_string.strip()))
+        for pattern in patterns:
+            if token_type == tokenize.COMMENT and pattern.search(token_string):
+                noqa_statements.append((start[0], token_string.strip()))  # noqa: PERF401
 
     return noqa_statements
